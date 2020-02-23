83c3c56791b13e2b01d35281f4cf357819c7f615
Nathan Shostek <szos@posteo.net>
2020-02-23 22:09:10 +0100

    added shell script to generate an xbps-graphical application

--- make-executable.sh
+++ make-executable.sh
@@ -2,12 +2,31 @@
 
 LISP=$(which sbcl)
 exit="$?"
-## echo "$LISP"
-## echo "$exit"
 
 if [ ! "$exit" -eq 0 ]; then
-    echo "NO SBCL"
+    /bin/echo -e "\e[1;31mSBCL not installed, exiting\e[0m"
     exit 1
+else
+    /bin/echo -e "\e[1;32mSBCL is installed, proceeding\e[0m"
 fi
 
-sbcl --eval "(ql:quickload :xbps-graphical)" --eval "(xbps-graphical::make-xbps-graphical-executable)"
+
+sbcl --eval "(progn (handler-case (in-package :ql) (PACKAGE-DOES-NOT-EXIST () (exit :code 1))) (sb-ext:exit :code 0))" > /dev/null 2>&1
+
+exit="$?"
+
+if [ ! "$exit" -eq 0 ]; then
+    /bin/echo -e "\e[1;31mQuicklisp not installed, exiting\e[0m"
+    exit 1
+else
+    /bin/echo -e "\e[1;32mQuicklisp is installed, proceeding\e[0m"
+fi
+
+sbcl --eval " (handler-case (ql:quickload :xbps-graphical) (quicklisp-client:system-not-found () (exit :code 1)))" --eval "(xbps-graphical::make-xbps-graphical-executable)"
+
+exit="$?"
+
+if [ ! "$exit" -eq 0 ]; then
+    /bin/echo -e "\e[1;31m:XBPS-GRAPHICAL not found, aborting\e[0m"
+    exit 1
+fi
